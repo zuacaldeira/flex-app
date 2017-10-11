0914a35455f74620afac575bd5b43b0d08668d3c
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2017-10-11 13:38:39 +0200

    Add test cases to articles repository.

--- src/test/java/data/ArticlesRepositoryTest.java
+++ src/test/java/data/ArticlesRepositoryTest.java
@@ -5,44 +5,81 @@
  */
 package data;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import db.FlexUser;
 import db.NewsArticle;
 import java.util.Collection;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
 
 /**
  *
  * @author zua
  */
+@RunWith(DataProviderRunner.class)
 public class ArticlesRepositoryTest {
 
     private final String TEST_USERNAME = "test:username";
     private final String TEST_PASSWORD = "test:password";
-    
-    
+
     public ArticlesRepositoryTest() {
+    }
+
+    @DataProvider
+    public static Object[][] loadNodes() {
+        return new Object[][]{
+            {DataProviderType.LATEST, null},
+            {DataProviderType.OLDEST, null},
+            {DataProviderType.CATEGORY, "business"},
+            {DataProviderType.FAKE, null},
+            {DataProviderType.FAVORITE, null},
+            {DataProviderType.READ, null},
+            {DataProviderType.FULL, null},
+            {DataProviderType.IMAGES_ONLY, null},
+            {DataProviderType.TITLES_ONLY, null},
+            {DataProviderType.COUNTRIES, "Portugal"},
+            {DataProviderType.LANGUAGES, "Portuguese"}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] captions() {
+        return new Object[][]{
+            {"Cat", "cat"},
+            {"", ""}
+        };
     }
 
     /**
      * Test of loadNodes method, of class ArticlesRepository.
+     *
+     * @param type
+     * @param value
      */
     @Test
-    public void testLoadNodes() {
+    @UseDataProvider("loadNodes")
+    public void testLoadNodes(DataProviderType type, String value) {
         System.out.println("loadNodes");
         ArticlesRepository instance = new ArticlesRepository();
         FlexUser user = new FlexUser(TEST_USERNAME, TEST_PASSWORD);
-        Collection<NewsArticle> result = instance.loadNodes(DataProviderType.LATEST, null, user);
+        Collection<NewsArticle> result = instance.loadNodes(type, value, user);
         assertNotNull(result);
     }
 
     /**
      * Test of getCategoryDBCaption method, of class ArticlesRepository.
+     *
+     * @param category
+     * @param expected
      */
     @Test
-    public void testGetCategoryDBCaption() {
+    @UseDataProvider("captions")
+    public void testGetCategoryDBCaption(String category, String expected) {
         System.out.println("getCategoryDBCaption");
-        assertEquals("cat", ArticlesRepository.getCategoryDBCaption("Cat"));
+        assertEquals(expected, ArticlesRepository.getCategoryDBCaption(category));
     }
 
     /**
@@ -53,5 +90,5 @@ public class ArticlesRepositoryTest {
         System.out.println("getSourceIdForSourceName");
         assertNull(ArticlesRepository.getSourceIdForSourceName("SourceName"));
     }
-    
+
 }
