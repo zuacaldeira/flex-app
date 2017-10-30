c9322f0427c657a0993be003a2fa6ec9f350729d
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2017-10-30 05:27:03 +0100

    Enter release preparation

--- release.sh
+++ release.sh
@@ -1,3 +1,7 @@
+git add .
+git commit -m "Enter release preparation"
+git push origin dev
+
 git checkout release
 git pull --rebase
 git stash
@@ -13,6 +17,7 @@ git checkout master
 git pull --rebase
 git stash
 git merge release
+git commit -m "Release to Production."
 git push origin master
 # TODO: unstash
 # Continue...
@@ -21,6 +26,11 @@ git checkout dev
 git pull --rebase
 git stash
 git merge release
+git commit -m "Enter new development cycle."
 git push origin dev
 # TODO: unstash
 # Continue...
+
+/Users/zua/Servers/glassfish4-latest/glassfish/bin/asadmin restart-domain domain1
+/Users/zua/Servers/glassfish4-latest/glassfish/bin/asadmin restart-domain production
+
