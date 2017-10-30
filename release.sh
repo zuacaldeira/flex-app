92717a24794de8b38cb2676d97d9f5696eac6dec
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2017-10-30 04:59:56 +0100

    Full cycle script.

--- release.sh
+++ release.sh
@@ -1,1 +1,26 @@
+git checkout release
+git pull --rebase
+git stash
+git merge dev
+# TODO: unstash
+# Continue...
 mvn release:clean release:prepare -Prelease
+git add .
+git commit -m "Release prepared."
+git push origin release
+
+git checkout master
+git pull --rebase
+git stash
+git merge release
+git push origin master
+# TODO: unstash
+# Continue...
+
+git checkout dev
+git pull --rebase
+git stash
+git merge release
+git push origin dev
+# TODO: unstash
+# Continue...
