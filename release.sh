f76821c1b33e5dca2de1d0db41c42a9719541e54
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2017-10-31 01:48:19 +0100

    Accepts argument to deploy to development script.

--- release.sh
+++ release.sh
@@ -2,18 +2,14 @@ echo "RELEASE"
 
 git checkout release
 git pull --rebase
-git stash
 git merge dev
 # TODO: unstash
 # Continue...
 mvn release:clean release:prepare -Prelease
-git add .
 git commit -m "Release prepared."
-git push origin release
 
 git checkout master
 git pull --rebase
-git stash
 git merge release
 git commit -m "Release to Production."
 git push origin master
@@ -22,7 +18,6 @@ git push origin master
 
 git checkout dev
 git pull --rebase
-git stash
 git merge release
 git commit -m "Enter new development cycle."
 git push origin dev
