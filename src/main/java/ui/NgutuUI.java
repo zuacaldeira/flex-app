7ca59087ea754bfecbc42cb5e0e4947619b612fd
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2017-12-05 22:13:45 +0100

    Removes auth0 authentication.

--- src/main/java/ui/NgutuUI.java
+++ src/main/java/ui/NgutuUI.java
@@ -1,11 +1,5 @@
 package ui;
 
-import com.auth0.exception.Auth0Exception;
-import org.ngutu.ui.auth0.NgutuAuthAPI;
-import com.auth0.json.auth.TokenHolder;
-import com.auth0.json.auth.UserInfo;
-import com.auth0.net.AuthRequest;
-import com.auth0.net.Request;
 import com.restfb.types.User;
 import com.vaadin.annotations.JavaScript;
 import com.vaadin.annotations.Push;
@@ -17,7 +11,6 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import db.AuthUserInfo;
 import db.FlexUser;
-import db.Gender;
 import db.Neo4jSessionFactory;
 import java.text.ParseException;
 import java.util.Locale;
@@ -26,7 +19,6 @@ import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
 import org.ngutu.ui.share.NgutuFacebookAPI;
 import services.FlexUserServiceInterface;
-import utils.MyDateUtils;
 import utils.ServiceLocator;
 
 /**
@@ -51,9 +43,7 @@ public class NgutuUI extends SecuredUI {
     public void init(VaadinRequest request) {
         if (request != null) {
             printRequest(request);
-            if (request.getParameterMap().containsKey("access_token")) {
-                handleAccessTokenRequest(request);
-            } else if (request.getParameterMap().containsKey("code")) {
+            if (request.getParameterMap().containsKey("code")) {
                 handleCodeRequest(request);
             }
         }
@@ -68,7 +58,11 @@ public class NgutuUI extends SecuredUI {
             NgutuFacebookAPI api = new NgutuFacebookAPI("");
             User user = api.fetchUserWithCode(code);
             printUserInfo(user);
-            getSession().setAttribute("user", convert2FlexUser(user));
+            try {
+                updateSession(convert2FlexUser(user));
+            } catch (ParseException ex) {
+                Logger.getLogger(NgutuUI.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
@@ -90,33 +84,12 @@ public class NgutuUI extends SecuredUI {
         System.out.printf("(%s, %s)\n", "Picture", user.getPicture().getUrl());
     }
 
-    private void printUserInfo(UserInfo userInfo) {
-        for (Object k : userInfo.getValues().keySet()) {
-            System.out.printf("(k, v) = (%s, %s)\n", k, userInfo.getValues().get(k));
-        }
-    }
-
     private void updateSession(FlexUser user) throws ParseException {
-
-        System.out.println("USER  --- " + user);
-        System.out.println("USER INFO --- " + user);
 
         FlexUserServiceInterface service = ServiceLocator.getInstance().findUserService();
         service.save(user);
         user = service.login(user.getUsername(), "null");
-
-        System.out.println("USER SAVED --- " + user);
-        System.out.println("USER INFO  SAVED --- " + user.getUserInfo());
-
         getSession().setAttribute("user", user);
-        System.out.println("SET ATTRIBUTE USER " + getSession().getAttribute("user"));
-    }
-
-    private FlexUser convert2FlexUser(UserInfo userInfo) {
-        AuthUserInfo authUserInfo = convertAuth0User2AuthUserInfo(userInfo);
-        FlexUser fuser = new FlexUser(authUserInfo.getSub(), null);
-        fuser.setUserInfo(authUserInfo);
-        return fuser;
     }
 
     private FlexUser convert2FlexUser(User user) {
@@ -126,30 +99,9 @@ public class NgutuUI extends SecuredUI {
         return fuser;
     }
 
-    private AuthUserInfo convertAuth0User2AuthUserInfo(UserInfo userInfo) {
-        AuthUserInfo authUserInfo = new AuthUserInfo();
-        authUserInfo.setSub((String) userInfo.getValues().get("sub"));
-        authUserInfo.setGender(Gender.valueOf(((String) userInfo.getValues().get("gender")).toUpperCase()));
-        authUserInfo.setEmailVerified((Boolean) userInfo.getValues().get("email_verified"));
-        try {
-            authUserInfo.setUpdatedAt(MyDateUtils.parseDate((String) userInfo.getValues().get("updated_at")));
-        } catch (ParseException ex) {
-            Logger.getLogger(NgutuUI.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        authUserInfo.setNickname((String) userInfo.getValues().get("nickname"));
-        authUserInfo.setName((String) userInfo.getValues().get("name"));
-        authUserInfo.setFamilyName((String) userInfo.getValues().get("family_name"));
-        authUserInfo.setGivenName((String) userInfo.getValues().get("given_name"));
-        authUserInfo.setLocale(new Locale((String) userInfo.getValues().get("locale")));
-        authUserInfo.setPicture((String) userInfo.getValues().get("picture"));
-        return authUserInfo;
-    }
-
     private AuthUserInfo convertFacebookUser2AuthUserInfo(User userInfo) {
         AuthUserInfo authUserInfo = new AuthUserInfo();
-        authUserInfo.setSub(userInfo.getId());
-        //authUserInfo.setGender(Gender.valueOf(userInfo.getGender()));
-        //authUserInfo.setEmailVerified(userInfo.getIsVerified());
+        authUserInfo.setSub(userInfo.getEmail());
         authUserInfo.setUpdatedAt(userInfo.getUpdatedTime());
         authUserInfo.setNickname(userInfo.getShortName());
         authUserInfo.setName(userInfo.getName());
@@ -158,41 +110,6 @@ public class NgutuUI extends SecuredUI {
         authUserInfo.setLocale(new Locale(userInfo.getLocale()));
         authUserInfo.setPicture(userInfo.getPicture().getUrl());
         return authUserInfo;
-    }
-
-    private void handleAccessTokenRequest(VaadinRequest request) {
-        String fragment = getNavigator().getState();
-        String accessToken = request.getParameter("access_token");
-        if (accessToken != null) {
-            System.out.println("AccessToken -> " + accessToken);
-        }
-    }
-
-    private void handleAuth0Request(VaadinRequest request) {
-        String fragment = getNavigator().getState();
-        String authorizationCode = request.getParameter("code");
-        if (authorizationCode != null) {
-            try {
-                UserInfo userInfo = extractUserInfo(fragment, authorizationCode);
-                updateSession(convert2FlexUser(userInfo));
-            } catch (Exception ex) {
-                Logger.getLogger(NgutuUI.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-
-    private UserInfo extractUserInfo(String fragment, String authorizationCode) throws Auth0Exception {
-        NgutuAuthAPI api = new NgutuAuthAPI(fragment);
-        AuthRequest authRequest = api.exchangeCode(authorizationCode, api.getRedirectUrl());
-        TokenHolder tokenHolder = authRequest.execute();
-
-        System.out.println("ACCESS_TOKEN --> " + tokenHolder.getAccessToken());
-
-        Request<UserInfo> req = api.userInfo(tokenHolder.getAccessToken());
-        UserInfo userInfo = req.execute();
-
-        System.out.println("NAME --> " + userInfo.getValues().get("name"));
-        return userInfo;
     }
 
     @WebServlet(urlPatterns = "/*", name = "NgutuUIServlet", asyncSupported = true)
