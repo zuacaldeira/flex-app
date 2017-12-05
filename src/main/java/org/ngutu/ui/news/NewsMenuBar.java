bfc737754a9a5772f5d17d9a1fedcc0a66552449
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2017-12-05 14:27:23 +0100

    Share on facebook call facebook api 3.

--- src/main/java/org/ngutu/ui/news/NewsMenuBar.java
+++ src/main/java/org/ngutu/ui/news/NewsMenuBar.java
@@ -66,6 +66,7 @@ public final class NewsMenuBar extends MenuBar {
         top.setStyleName("menu-bar-top");
         if (getUser() == null) {
             login = top.addItem("Login", VaadinIcons.SIGN_IN, (selectedItem) -> {
+                // Facebook login
                 NgutuFacebookAPI authAPI = new NgutuFacebookAPI(getUI().getNavigator().getState());
                 authAPI.authorize();
             });
